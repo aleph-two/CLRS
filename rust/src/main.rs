faf4f9c0dd88efce519a1441bd3ae74e4f344012
Fraguinha <joaoluisfreirefraga@gmail.com>
2021-08-03 12:28:33 +0100

    Add rust version

---
+++ rust/src/main.rs
@@ -0,0 +1,12 @@
+use sorting::insertion_sort;
+
+mod helper;
+mod sorting;
+
+fn main() {
+    let size = 100_000;
+    let mut a = helper::random_vector(size);
+
+    print!("insertion sort: ");
+    helper::benchmark(insertion_sort::sort, &mut a, size);
+}
