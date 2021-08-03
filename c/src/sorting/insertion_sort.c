f3e4cf4a4c3e749d892dde4bb4ec712c3dea3d16
Fraguinha <joaoluisfreirefraga@gmail.com>
2021-08-03 17:53:06 +0100

    Update insertion sort

--- c/src/sorting/insertion_sort.c
+++ c/src/sorting/insertion_sort.c
@@ -1,14 +1,11 @@
 int *insertion_sort(int *a, int size) {
-  int key, i;
   for (int j = 1; j < size; j++) {
-    key = a[j];
-    // insert a[j] into the sorted sequence a[0 .. j - 1]
-    i = j - 1;
-    while (i >= 0 && a[i] > key) {
+    int i = j - 1;
+    while (i >= 0 && a[i] > a[j]) {
       a[i + 1] = a[i];
       i--;
     }
-    a[i + 1] = key;
+    a[i + 1] = a[j];
   }
   return a;
 }
